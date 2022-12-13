ba77155bb95db99c7c9c43da1c25f4450976e350
odraudee <111029975+odraudee@users.noreply.github.com>
2022-12-12 22:03:16 -0600

    add files for exercise 1

---
+++ EjercicioTema1/src/com/ejercicio/Main.java
@@ -0,0 +1,18 @@
+package com.ejercicio;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 4;
+        System.out.println(numero);
+        long numLong = 1234567890;
+        System.out.println(numLong);
+        double numDouble = 5.1;
+        System.out.println(numDouble);
+        boolean varBool = true;
+        System.out.println(varBool);
+        char varChar = 'A';
+        System.out.println(varChar);
+        String texto = "Esto es un texto String";
+        System.out.println(texto);
+    }
+}
